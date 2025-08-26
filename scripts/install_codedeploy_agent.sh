64acad9e9ceabf6834b4961433f764cc23522237
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2025-08-26 12:13:42 +0530

    Update install_codedeploy_agent.sh

--- scripts/install_codedeploy_agent.sh
+++ scripts/install_codedeploy_agent.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Install CodeDeploy agent
 echo "Installing CodeDeploy agent..."
-yum install -y ruby wget
-cd /home/ec2-user
+yum install -y ruby
+cd /tmp
 wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
-service codedeploy-agent start
-echo "CodeDeploy agent installed and started."
+systemctl start codedeploy-agent
+systemctl enable codedeploy-agent
