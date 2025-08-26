d4a41d398ce070fcc45d60b5dba611969433ee07
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2025-08-26 12:13:12 +0530

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Deploy WAR and start Tomcat
-echo "Deploying WAR..."
-cp /opt/tomcat/webapps/app.war /opt/tomcat/webapps/
-echo "Starting Tomcat server..."
-/opt/tomcat/bin/shutdown.sh
-/opt/tomcat/bin/startup.sh
-echo "Tomcat started and app deployed."
+echo "Starting Tomcat..."
+systemctl start tomcat
+systemctl enable tomcat
