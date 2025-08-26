0070400ea3a93fc4c58bb5410731e5854286eb11
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2025-08-26 12:12:51 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# Install Java & Tomcat
-echo "Installing Java and Tomcat..."
-yum install -y java-17-amazon-corretto
-yum install -y wget tar
-cd /opt
-wget https://downloads.apache.org/tomcat/tomcat-10/v10.1.15/bin/apache-tomcat-10.1.15.tar.gz
-tar xvf apache-tomcat-10.1.15.tar.gz
-mv apache-tomcat-10.1.15 tomcat
-chmod +x /opt/tomcat/bin/*.sh
-echo "Java and Tomcat installed."
+echo "Installing dependencies..."
+yum update -y
+yum install -y java-17-amazon-corretto wget unzip
