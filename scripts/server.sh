ccae655193c7e143c27420fec8e57e01e4e55340
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2025-08-26 11:55:41 +0530

    Update server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-WAR_FILE=/opt/tomcat/webapps/app.war
-if [ -f "$WAR_FILE" ]; then
-    sudo systemctl stop tomcat
-    sudo cp target/app.war /opt/tomcat/webapps/
-    sudo systemctl start tomcat
-fi
+# Deploy WAR and start Tomcat
+echo "Deploying WAR..."
+cp /opt/tomcat/webapps/app.war /opt/tomcat/webapps/
+echo "Starting Tomcat server..."
+/opt/tomcat/bin/shutdown.sh
+/opt/tomcat/bin/startup.sh
+echo "Tomcat started and app deployed."
